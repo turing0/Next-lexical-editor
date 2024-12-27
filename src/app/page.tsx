bdac41da9af02c6c8b7771a55f67eaf54f9793e4
unknown <turing175@gmail.com>
2024-12-27 17:01:24 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { ModeToggle } from "@/components/shared-components/ModeToggle";
 export default function Home() {
   return (
     <div className=" container max-w-7xl mx-auto h-full max-sm:px-0  md:px-20 ">
-      <h1 className="text-3xl text-center font-bold">Editor</h1>
-      <ModeToggle/>
+      {/* <h1 className="text-3xl text-center font-bold">Editor</h1> */}
+      {/* <ModeToggle/> */}
       <div className="max-sm:px-2">
-      <LexicalEditor SavelocalStorage={true} loadFromLocalStorge={true}/>
+        <LexicalEditor SavelocalStorage={true} loadFromLocalStorge={true}/>
 
       </div>
     </div>
